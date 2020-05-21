c4db9be1db3545639e1ecea2c918d7a90ddcac28
Stefan Wyss <wyss@superspider.net>
2020-05-21 10:50:44 +0200

    Added serial-peripheral.ino

---
+++ serial-peripheral/serial-peripheral.ino
@@ -0,0 +1,64 @@
+/*
+  Serial Peripheral, 21.05.2020, wyss@superspider.net
+
+  This example creates a BLE peripheral with service that contains a
+  characteristic to receive some characters and sends them out in the
+  serial port.
+
+  The circuit:
+  - Arduino Nano 33 BLE
+
+  You can use a generic BLE central app, like LightBlue (iOS and Android) or
+  nRF Connect (Android), to interact with the services and characteristics
+  created in this sketch.
+*/
+
+#include <ArduinoBLE.h>
+#define BUFSIZE 64
+
+const int ledPin = LED_BUILTIN;     // set ledPin to on-board LED
+
+BLEService comService("19B10010-E8F2-537E-4F6C-D104768A1214"); // create a new BLE COM service
+
+// create com characteristic and allow remote device to write & get notifications
+BLECharacteristic comCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLEWrite | BLENotify, BUFSIZE);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  pinMode(ledPin, OUTPUT);          // use the LED as an output
+
+  if (!BLE.begin())                 // initialize the BLE device
+  { // 1 on success, 0 on failure
+    Serial.println("Failed to start BLE!");
+    while (1);                      // only wait out is to kill execution
+  }
+
+  BLE.setLocalName("BLECOM"); // set the local value used when advertising
+  BLE.setAdvertisedService(comService);// set the advertised service UUID used when advertising to the value of the BLEService provided
+  comService.addCharacteristic(comCharacteristic);// add the characteristics to the service
+  BLE.addService(comService);       // add the service
+
+  // comCharacteristic.writeValue(0);
+  BLE.advertise();                  // start advertising
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  int len = 0;
+  byte rx[BUFSIZE];
+  char print_buf[BUFSIZE];
+
+  BLE.poll();                       // poll for BLE events
+  if (comCharacteristic.written())
+  {
+    // central has written to com characteristic
+    len = comCharacteristic.readValue(rx, BUFSIZE);
+    for (int i = 0; i < len; i++)
+    {
+      sprintf(print_buf, "%c", rx[i]);
+      Serial.print(print_buf);
+    }
+  }
+}
